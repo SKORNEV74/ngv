e692d3bb83bff803d9430fa9026e4c040638fc19
SKORNEV74 <yasher7495@yandex.ru>
2022-07-05 23:28:34 +0300

    first commit, where the main task was solved

---
+++ scripts.js
@@ -0,0 +1,44 @@
+// Список курсов
+let courses = [
+    { name: "Courses in England", prices: [0, 100] },
+    { name: "Courses in Germany", prices: [500, null] },
+    { name: "Courses in Italy", prices: [100, 200] },
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+    { name: "Courses in France", prices: [null, null] },
+];
+
+// Функция фильтрации
+function Filtering(requiredRange) {
+    console.log("New filters:")
+    for (let i = 0; i < courses.length; i++) {
+        if (requiredRange[0] === null && requiredRange[1] === null) {
+            console.log(courses[i].name);
+        } else if (requiredRange[0] === null) {
+            if (requiredRange[1] >= courses[i].prices[0]) {
+                console.log(courses[i].name);
+            }
+        } else if (requiredRange[1] === null) {
+            if (requiredRange[0] <= courses[i].prices[1] || courses[i].prices[1] == null) {
+                console.log(courses[i].name);
+            }
+        } else {
+            if (requiredRange[1] >= courses[i].prices[0] &&
+                (requiredRange[0] <= courses[i].prices[1] || courses[i].prices[1] == null)) {
+                console.log(courses[i].name);
+            }
+        }
+    }
+}
+
+// Варианты цен (фильтры), которые ищет пользователь
+let requiredRange1 = [null, 200];
+let requiredRange2 = [100, 350];
+let requiredRange3 = [200, null];
+
+// [подходящие курсы для каждого варианта фильтра]
+Filtering(requiredRange1);
+Filtering(requiredRange2);
+Filtering(requiredRange3);
